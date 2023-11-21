c45f409201dfddf6e1658d54b9dba1be04c34788
mrbillgates2001 <shahboznabiyev2001@gmail.com>
2023-11-22 04:41:14 +0500

    Header added

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// BURGER MENU //////////////////////////////////////////
+
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+
+// NAVBAR SHRINK ///////////////////////////////////////
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "5px 0";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "5px 0";
+    document.getElementById("logo").style.fontSize = "25px";
+  }
+}
+
+// DARK_LIGHT //////////////////////////////////////////
+
+// let modeBtn = document.getElementById("dark-light");
+
+// modeBtn.addEventListener("click", function () {
+//   if (document.body.className != "light") {
+//     this.firstElementChild.src = "./images/light.svg";
+//   } else {
+//     this.firstElementChild.src = "./images/dark.svg";
+//   }
+//   document.body.classList.toggle("light");
+// });
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
