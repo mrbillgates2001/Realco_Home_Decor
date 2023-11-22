edb2dfa39f59ea6b629e62dde0b3c75991808cfe
mrbillgates2001 <shahboznabiyev2001@gmail.com>
2023-11-22 14:33:15 +0500

    main created

--- js/main.js
+++ js/main.js
@@ -26,17 +26,6 @@ function scrollFunction() {
 
 // DARK_LIGHT //////////////////////////////////////////
 
-// let modeBtn = document.getElementById("dark-light");
-
-// modeBtn.addEventListener("click", function () {
-//   if (document.body.className != "light") {
-//     this.firstElementChild.src = "./images/light.svg";
-//   } else {
-//     this.firstElementChild.src = "./images/dark.svg";
-//   }
-//   document.body.classList.toggle("light");
-// });
-
 let modeBtn = document.getElementById("dark-light");
 
 modeBtn.addEventListener("click", function () {
